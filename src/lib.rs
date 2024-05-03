ddccd182938d511a373f8bd097ee56bd62468473
glyph <glyph@mycelial.technology>
2024-05-03 10:21:17 +0100

    add Queue trait implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
+#![feature(allocator_api)]
+#![feature(maybe_uninit_slice)]
+#![feature(maybe_uninit_write_slice)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use core::cmp::min;
+use core::mem::MaybeUninit;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub trait Queue {
+    type Item: Copy;
+    type Error;
+
+    fn get_amount(&self) -> usize;
+
+    fn enqueue(&mut self, item: Self::Item) -> Result<(), Self::Error>;
+
+    fn enqueue_slots(&mut self) -> Result<&mut [MaybeUninit<Self::Item>], Self::Error>;
+
+    unsafe fn did_enqueue(&mut self, amount: usize) -> Result<(), Self::Error>;
+
+    fn bulk_enqueue(&mut self, buffer: &[Self::Item]) -> Result<usize, Self::Error> {
+        let slots = self.enqueue_slots()?;
+        let amount = min(slots.len(), buffer.len());
+        MaybeUninit::copy_from_slice(&mut slots[..amount], &buffer[..amount]);
+        unsafe {
+            self.did_enqueue(amount)?;
+        }
+
+        Ok(amount)
+    }
+
+    fn dequeue(&mut self) -> Result<Self::Item, Self::Error>;
+
+    fn dequeue_slots(&mut self) -> Result<&[Self::Item], Self::Error>;
+
+    fn did_dequeue(&mut self, amount: usize) -> Result<(), Self::Error>;
+
+    fn bulk_dequeue(
+        &mut self,
+        buffer: &mut [MaybeUninit<Self::Item>],
+    ) -> Result<usize, Self::Error> {
+        let slots = self.dequeue_slots()?;
+        let amount = min(slots.len(), buffer.len());
+        MaybeUninit::copy_from_slice(&mut buffer[..amount], &slots[..amount]);
+        self.did_dequeue(amount)?;
+
+        Ok(amount)
     }
 }
