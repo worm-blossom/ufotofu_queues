bda04a51d7c9cdeb352f96f4085970bd0b8b25e4
glyph <glyph@mycelial.technology>
2024-05-17 08:53:18 +0100

    refactor API to use Option return type and update tests

--- src/fixed.rs
+++ src/fixed.rs
@@ -7,15 +7,6 @@ use core::mem::MaybeUninit;
 
 use crate::Queue;
 
-/// A fixed queue error.
-#[derive(Debug, PartialEq, Eq)]
-pub enum FixedQueueError {
-    /// No items are available.
-    Empty,
-    /// All available capacity is occupied.
-    Full,
-}
-
 /// A queue holding up to a certain number of items. The capacity is set upon
 /// creation and remains fixed. Performs a single heap allocation on creation.
 #[derive(Debug)]
@@ -53,7 +44,7 @@ impl<T: Copy, A: Allocator> Fixed<T, A> {
         self.read + self.amount < self.capacity()
     }
 
-    /// Return a readable slice from the queue, which may or may not be contiguous.
+    /// Return a readable slice from the queue.
     fn readable_slice(&mut self) -> &[MaybeUninit<T>] {
         if self.is_data_contiguous() {
             &self.data[self.read..self.write_to()]
@@ -62,7 +53,7 @@ impl<T: Copy, A: Allocator> Fixed<T, A> {
         }
     }
 
-    /// Return a writeable slice from the queue, which may or may not be contiguous.
+    /// Return a writeable slice from the queue.
     fn writeable_slice(&mut self) -> &mut [MaybeUninit<T>] {
         let capacity = self.capacity();
         if self.is_data_contiguous() {
@@ -72,7 +63,7 @@ impl<T: Copy, A: Allocator> Fixed<T, A> {
         }
     }
 
-    fn capacity(&self) -> usize {
+    pub fn capacity(&self) -> usize {
         self.data.len()
     }
 
@@ -83,43 +74,40 @@ impl<T: Copy, A: Allocator> Fixed<T, A> {
 
 impl<T: Copy, A: Allocator> Queue for Fixed<T, A> {
     type Item = T;
-    type Error = FixedQueueError;
 
     /// Return the amount of items in the queue.
-    fn get_amount(&self) -> usize {
+    fn amount(&self) -> usize {
         self.amount
     }
 
     /// Attempt to enqueue the next item.
     ///
-    /// Will return an error if the queue is full at the time of calling.
-    fn enqueue(&mut self, item: T) -> Result<(), FixedQueueError> {
+    /// Will return the item if the queue is full at the time of calling.
+    fn enqueue(&mut self, item: T) -> Option<T> {
         if self.amount == self.capacity() {
-            Err(FixedQueueError::Full)
+            Some(item)
         } else {
             self.data[self.write_to()].write(item);
             self.amount += 1;
 
-            Ok(())
+            None
         }
     }
 
     /// Expose a non-empty slice of memory for the client code to fill with items that should
     /// be enqueued.
     ///
-    /// Will return an error if the queue is full at the time of calling.
-    fn enqueue_slots(&mut self) -> Result<&mut [MaybeUninit<T>], FixedQueueError> {
-        // TODO: Can the amount ever be greater than capacity?
-        if self.amount >= self.capacity() {
-            Err(FixedQueueError::Full)
+    /// Will return `None` if the queue is full at the time of calling.
+    fn enqueue_slots(&mut self) -> Option<&mut [MaybeUninit<T>]> {
+        if self.amount == self.capacity() {
+            None
         } else {
-            Ok(self.writeable_slice())
+            Some(self.writeable_slice())
         }
     }
 
-    /// Instruct the queue to consume the first `amount` many items of the `enqueue_slots`
-    /// it has most recently exposed. The semantics must be equivalent to those of `enqueue`
-    /// being called `amount` many times with exactly those items.
+    /// Inform the queue that `amount` many items have been written to the first `amount`
+    /// indices of the `enqueue_slots` it has most recently exposed.
     ///
     /// #### Invariants
     ///
@@ -128,55 +116,50 @@ impl<T: Copy, A: Allocator> Queue for Fixed<T, A> {
     ///
     /// #### Safety
     ///
-    /// Callers may assume the first `amount` many `enqueue_slots` that were most recently
+    /// The queue will assume the first `amount` many `enqueue_slots` that were most recently
     /// exposed to contain initialized memory after this call, even if the memory it exposed was
-    /// originally uninitialized. Violating the invariants can cause the queue to read undefined
+    /// originally uninitialized. Violating the invariants will cause the queue to read undefined
     /// memory, which triggers undefined behavior.
-    unsafe fn did_enqueue(&mut self, amount: usize) -> Result<(), FixedQueueError> {
+    unsafe fn did_enqueue(&mut self, amount: usize) {
         self.amount += amount;
-
-        Ok(())
     }
 
     /// Attempt to dequeue the next item.
     ///
-    /// Will return an error if the queue is empty at the time of calling.
-    fn dequeue(&mut self) -> Result<T, FixedQueueError> {
+    /// Will return `None` if the queue is empty at the time of calling.
+    fn dequeue(&mut self) -> Option<T> {
         if self.amount == 0 {
-            Err(FixedQueueError::Empty)
+            None
         } else {
             let previous_read = self.read;
             // Advance the read index by 1 or reset to 0 if at capacity.
             self.read = (self.read + 1) % self.capacity();
             self.amount -= 1;
 
-            Ok(unsafe { self.data[previous_read].assume_init() })
+            Some(unsafe { self.data[previous_read].assume_init() })
         }
     }
 
-    /// Expose a non-empty slice of items to be dequeued (or an error).
-    /// The items in the slice must not have been emitted by `dequeue` before.
+    /// Expose a non-empty slice of items to be dequeued.
     ///
-    /// Will return an error if the queue is empty at the time of calling.
-    fn dequeue_slots(&mut self) -> Result<&[T], FixedQueueError> {
+    /// Will return `None` if the queue is empty at the time of calling.
+    fn dequeue_slots(&mut self) -> Option<&[T]> {
         if self.amount == 0 {
-            Err(FixedQueueError::Empty)
+            None
         } else {
-            Ok(unsafe { MaybeUninit::slice_assume_init_ref(self.readable_slice()) })
+            Some(unsafe { MaybeUninit::slice_assume_init_ref(self.readable_slice()) })
         }
     }
 
-    /// Mark `amount` many items as having been dequeued. Future calls to `dequeue` and to
-    /// `dequeue_slots` must act as if `dequeue` had been called `amount` many times.
+    /// Mark `amount` many items as having been dequeued.
     ///
     /// #### Invariants
     ///
-    /// Callers must not mark items as dequeued that had not previously been exposed by `dequeue_slots`.
-    fn did_dequeue(&mut self, amount: usize) -> Result<(), FixedQueueError> {
+    /// The queue must not mark items as dequeued that had not previously been exposed by
+    /// `dequeue_slots`.
+    fn did_dequeue(&mut self, amount: usize) {
         self.read = (self.read + amount) % self.capacity();
         self.amount -= amount;
-
-        Ok(())
     }
 }
 
@@ -188,17 +171,17 @@ mod tests {
     fn enqueues_and_dequeues_with_correct_amount() {
         let mut queue: Fixed<u8> = Fixed::new(4);
 
-        queue.enqueue(7).unwrap();
-        queue.enqueue(21).unwrap();
-        queue.enqueue(196).unwrap();
-        assert_eq!(queue.get_amount(), 3);
+        let _ = queue.enqueue(7);
+        let _ = queue.enqueue(21);
+        let _ = queue.enqueue(196);
+        assert_eq!(queue.amount(), 3);
 
-        queue.enqueue(233).unwrap();
-        assert_eq!(queue.get_amount(), 4);
+        let _ = queue.enqueue(233);
+        assert_eq!(queue.amount(), 4);
 
         // Queue should be first-in, first-out.
-        assert_eq!(queue.dequeue(), Ok(7));
-        assert_eq!(queue.get_amount(), 3);
+        assert_eq!(queue.dequeue(), Some(7));
+        assert_eq!(queue.amount(), 3);
     }
 
     #[test]
@@ -206,34 +189,35 @@ mod tests {
         let mut queue: Fixed<u8> = Fixed::new(4);
         let mut buf: [MaybeUninit<u8>; 4] = MaybeUninit::uninit_array();
 
-        let amount = queue.bulk_enqueue(b"ufo");
-        assert_eq!(amount.unwrap(), 3);
+        let enqueue_amount = queue.bulk_enqueue(b"ufo");
+        let dequeue_amount = queue.bulk_dequeue(&mut buf);
 
-        let amount = queue.bulk_dequeue(&mut buf);
-        assert_eq!(amount.unwrap(), 3);
+        assert_eq!(enqueue_amount, dequeue_amount);
     }
 
     #[test]
-    fn errors_on_enqueue_when_queue_is_full() {
+    fn returns_item_on_enqueue_when_queue_is_full() {
         let mut queue: Fixed<u8> = Fixed::new(1);
 
-        queue.enqueue(7).unwrap();
+        let _ = queue.enqueue(7);
 
-        assert_eq!(queue.enqueue(0).unwrap_err(), FixedQueueError::Full);
+        assert_eq!(queue.enqueue(0), Some(0))
     }
 
     #[test]
-    fn errors_on_dequeue_when_queue_is_empty() {
+    fn returns_none_on_dequeue_when_queue_is_empty() {
         let mut queue: Fixed<u8> = Fixed::new(1);
 
-        queue.enqueue(7).unwrap();
-        queue.dequeue().unwrap();
+        // Enqueue and then dequeue an item.
+        let _ = queue.enqueue(7);
+        let _ = queue.dequeue();
 
-        assert_eq!(queue.dequeue().unwrap_err(), FixedQueueError::Empty);
+        // The queue is now empty.
+        assert!(queue.dequeue().is_none());
     }
 
     #[test]
-    fn errors_on_enqueue_slots_when_none_are_available() {
+    fn returnes_none_on_enqueue_slots_when_none_are_available() {
         // Create a fixed queue that exposes four slots.
         let mut queue: Fixed<u8> = Fixed::new(4);
 
@@ -242,32 +226,32 @@ mod tests {
         let slots = queue.enqueue_slots().unwrap();
         MaybeUninit::copy_from_slice(&mut slots[0..2], &data[0..2]);
         unsafe {
-            assert!(queue.did_enqueue(2).is_ok());
+            queue.did_enqueue(2);
         }
 
         // Copy data to two of the available slots and call `did_enqueue`.
         let slots = queue.enqueue_slots().unwrap();
         MaybeUninit::copy_from_slice(&mut slots[0..2], &data[0..2]);
         unsafe {
-            assert!(queue.did_enqueue(2).is_ok());
+            queue.did_enqueue(2);
         }
 
         // Make a third call to `enqueue_slots` after all available slots have been used.
-        assert_eq!(queue.enqueue_slots().unwrap_err(), FixedQueueError::Full);
+        assert!(queue.enqueue_slots().is_none());
     }
 
     #[test]
-    fn errors_on_dequeue_slots_when_none_are_available() {
+    fn returns_none_on_dequeue_slots_when_none_are_available() {
         // Create a fixed queue that exposes four slots.
         let mut queue: Fixed<u8> = Fixed::new(4);
 
         let data = b"tofu";
-        let _amount = queue.bulk_enqueue(data).unwrap();
+        let _amount = queue.bulk_enqueue(data);
 
         let _slots = queue.dequeue_slots().unwrap();
-        assert!(queue.did_dequeue(4).is_ok());
+        queue.did_dequeue(4);
 
         // Make a second call to `dequeue_slots` after all available slots have been used.
-        assert_eq!(queue.dequeue_slots().unwrap_err(), FixedQueueError::Empty);
+        assert!(queue.dequeue_slots().is_none());
     }
 }
